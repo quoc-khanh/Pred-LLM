309ced904a532779a20cdfa485248e13311e5c65
Quoc Khanh <trandamquockhanh123@gmail.com>
2025-03-13 12:42:16 +0700

    Update pred_llm.py

--- Compare data generation/pred_llm.py
+++ Compare data generation/pred_llm.py
@@ -36,14 +36,15 @@ logging.basicConfig(
 def get_score(train_data, test_data, target_col, best_params):
     train_x = train_data.drop(columns=target_col).copy()
     test_x = test_data.drop(columns=target_col).copy()
-    train_y = train_data[[target_col]]
-    test_y = test_data[[target_col]]
+    train_y = train_data[target_col]
+    test_y = test_data[target_col]
     train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size=0.2, random_state=1)
-    gbm = lgb.LGBMRegressor(**best_params)
+    gbm = lgb.LGBMClassifier(**best_params)
     gbm.fit(train_x, train_y, eval_set=[(val_x, val_y)], callbacks=[lgb.early_stopping(50, verbose=False)])
-    pred = pd.DataFrame(gbm.predict(test_x), index=test_x.index)
+    pred = gbm.predict(test_x)
     score = f1_score(test_y, pred)
     return score, gbm
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default="iris", type=str, nargs='?', help='dataset name')
