23d8d92f1277e8f55e068a7c82654850fd0c40a8
Quoc Khanh <trandamquockhanh123@gmail.com>
2025-03-17 20:33:36 +0700

    Update pred_llm.py

--- Compare data generation/pred_llm.py
+++ Compare data generation/pred_llm.py
@@ -7,6 +7,8 @@ from tabular_llm.predllm_utils import _encode_row_partial
 from tabular_llm.predllm import PredLLM
 import read_data
 import torch
+import tempfile
+import os
 
 ###TAPTAP
 from TapTap_master.taptap.taptap import Taptap
@@ -17,6 +19,7 @@ from sklearn.model_selection import train_test_split
 
 ###GREAT
 from be_great import GReaT
+from P-TA-main.main import train_pipeline
 
 ###CTGAN COPULA_GAN TVAE 
 from sdv.single_table import CTGANSynthesizer
@@ -175,8 +178,14 @@ for dataset in datasets:
                     #     np.save(f, X_train_new_ORI)
                     # with open("./results/_classification/{}/y_gen_ORI_{}.npz".format(method, file_name), "wb") as f:
                     #     np.save(f, y_train_new_ORI)
-                if method == "pred_llm_dpo":continue
-    
+                if method == "great_dpo":          
+                    # Tạo một file CSV tạm thời. delete=False để file không bị xóa khi đóng.
+                    tmp_file = tempfile.NamedTemporaryFile(suffix=".csv", delete=False)
+                    csv_path = tmp_file.name  # Lấy đường dẫn trước khi đóng file
+                    tmp_file.close()  # Đóng file để tránh lỗi khi ghi dữ liệu
+                
+                    X_y_train_df.to_csv(csv_path, index=False)  # Ghi DataFrame vào file CSV
+                    X_y_train_new = train_pipeline(csv_path = csv_path)
                 if method == "taptap":
                     train_data, test_data = X_y_train_df, X_y_test_df
                     target_col = 'target'
